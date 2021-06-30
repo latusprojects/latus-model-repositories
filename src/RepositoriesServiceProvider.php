78df387efef8e4d19c11503a761f538c5b3f17ca
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-06-30 17:10:15 +0200

    Add composer.json and RepositoriesServiceProvider.php

---
+++ src/RepositoriesServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Latus\Repositories;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoriesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
