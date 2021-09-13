c0832f85e7966862244255f733d47a7aa31050b5
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-13 22:09:38 +0200

    Add phpDoc segments to methods

--- src/Contracts/Repository.php
+++ src/Contracts/Repository.php
@@ -9,10 +9,27 @@ use Illuminate\Database\Eloquent\Model;
 interface Repository
 {
 
+    /**
+     * Gets an instance of the related model
+     *
+     * @return Model
+     */
     public function relatedModel(): Model;
 
+    /**
+     * Attempts to create a new model
+     *
+     * @param array $attributes
+     * @return Model
+     */
     public function create(array $attributes): Model;
 
+    /**
+     * Attempts to find a model by its id
+     *
+     * @param int|string $id
+     * @return Model|null
+     */
     public function find(int|string $id): Model|null;
 
 }
